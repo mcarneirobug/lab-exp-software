6e1607c20f1d032481193e60a1f30d3b8eafcabd
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-02-15 17:29:28 -0300

    feat: Added dotenv to get the token

--- Sprint 1 - Lab01S01/__main__.py
+++ Sprint 1 - Lab01S01/__main__.py
@@ -1,12 +1,11 @@
 import json
+from json import dumps
 import sys
 import os
-from dotenv import load_dotenv, find_dotenv
 from Mining import Mining
-
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-SECRET_KEY = os.getenv("TOKEN")
 
-mining = Mining("5fa50a6904490d2b3d04f254e4f36339b1e02226")
+mining = Mining(os.getenv("TOKEN"))
 print(json.dumps(mining.run_github_query(), sort_keys=True, indent=4))
